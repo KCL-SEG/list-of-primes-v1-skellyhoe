f2aa5e0b7e8af770fae93e3e8883d039f4e8dea0
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-09-30 16:33:27 +0100

    Add functionality for primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    i = 0
+
+    while i < number_of_primes:
+        prime = True
+        for j in range(2, num):
+            if num % j == 0:
+                prime = False
+
+        if prime:
+            list.append(num)
+            i = i + 1
+
+        num = num + 1
     return list
